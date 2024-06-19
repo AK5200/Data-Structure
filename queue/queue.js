6c77ed3a71ec21045f9309979894a7ec436ca86f
Anupam Kumar <anupamkmr5200@gmail.com>
2024-06-19 13:31:03 +0530

    [Anupam] added: Queue

--- queue/queue.js
+++ queue/queue.js
@@ -1,26 +1,106 @@
 // QUEUE
 // FIFO
 //    F ----- R
-// we not need to check overflow condition, as array are dynamic
+// no need to check overflow condition, as array are dynamic
 
-class Queue
+// class Queue
+// {
+//     constructor()
+//     {
+//         this.queue = [];
+//     }
+
+//     enqueue(element)
+//     {
+//         this.queue.push(element);
+//     }
+
+//     dequeue()
+//     {
+//         if(this.isEmpty())
+//             throw new Error("Empty");
+
+//         return this.queue.shift();
+//     }
+
+//     isEmpty()
+//     {
+//         return this.queue.length === 0;
+//     }
+
+//     size()
+//     {
+//         return this.queue.length;
+//     }
+
+//     front()
+//     {
+//         return this.queue[0];
+//     }
+
+//     print()
+//     {
+//         for(let i=0;i< this.queue.length; i++)
+//             {
+//                 console.log(this.queue[i]);
+//             }
+//     }
+
+// }
+
+//     const myQueue = new Queue();
+
+//     myQueue.enqueue(5);
+//     myQueue.enqueue(3);
+//     myQueue.enqueue(2);
+//     myQueue.dequeue();
+//     console.log(myQueue.print());
+//     console.log(myQueue.size());
+
+
+
+// CIRCULAR QUEUE
+// fixed size
+
+class CircularQueue
 {
-    constructor()
+    constructor(k)
     {
-        this.queue = [];
+        this.queue = []
+        this.size = k;
     }
 
-    enqueue(element)
+    enQueue(element)
     {
+        if(this.size === this.queue.length)
+            throw new Error("queue is full");
+
         this.queue.push(element);
     }
 
-    dequeue()
+    deQueue()
     {
-        if(this.isEmpty())
-            throw new Error("Empty");
+        if(this.queue.length == 0)
+            throw new Error("Queue is empty");
 
-        return this.queue.shift();
+        this.queue.shift();
+    }
+
+
+    front()
+    {
+        if(this.queue.length === 0)
+            throw new Error("Queue is empty");
+
+        return this.queue[0];
+    }
+
+    rear()
+    {
+        if(this.queue.length === 0)
+            throw new Error("Queue is empty");
+
+        return this.queue[this.queue.length-1];
     }
 
     isEmpty()
@@ -28,30 +108,21 @@ class Queue
         return this.queue.length === 0;
     }
 
-    size()
+    isFull()
     {
-        return this.queue.length;
+        return this.queue.length === this.size;
     }
-
-    front()
-    {
-        return this.queue[0];
-    }
-
-    print()
-    {
-        for(let i=0;i< this.queue.length; i++)
-            {
-                console.log(this.queue[i]);
-            }
-    }
-
 }
 
-    const myQueue = new Queue();
 
-    myQueue.enqueue(5);
-    myQueue.enqueue(3);
-    myQueue.enqueue(2);
-    myQueue.dequeue();
-    console.log(myQueue.print());
+const cqueue = new CircularQueue(3);
+
+cqueue.enQueue(3);
+cqueue.enQueue(4);
+cqueue.enQueue(5);
+
+
+cqueue.deQueue();
+cqueue.enQueue(1);
+console.log(cqueue.front());
+console.log(cqueue.rear());
